5cf49c0e79dc6ba668f698d1b43231548b409c44
SHIVAM-AIML <shivam.sharma19sept@gmail.com>
2021-05-28 20:30:44 +0530

    Added page.html

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/covid19')
+def covid19():
+    return render_template('page.html')
+
+
 app.run(debug=True)
