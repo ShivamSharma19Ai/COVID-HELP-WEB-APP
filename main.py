5b3622cef681128bb0926299921fe224990c9f06
SHIVAM-AIML <shivam.sharma19sept@gmail.com>
2021-05-30 03:34:17 +0530

    added login and register page

--- main.py
+++ main.py
@@ -15,5 +15,12 @@ def covid19():
 def posts():
     return render_template('posts.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 app.run(debug=True)
